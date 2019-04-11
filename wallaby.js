52a01656cc0e8e0b929b4c1d6f9228984a2e0a1b
Philippe Matray <phmatray@gmail.com>
2019-04-11 19:45:28 +0200

    configure wallaby

---
+++ wallaby.js
@@ -0,0 +1,30 @@
+module.exports = function(wallaby) {
+  return {
+    files: ['tsconfig.json', 'src/**/*.ts?(x)', '!src/**/*.spec.ts?(x)'],
+
+    tests: ['src/**/*.test.ts?(x)'],
+
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+
+    testFramework: 'jest',
+
+    compilers: {
+      '**/*.ts?(x)': wallaby.compilers.typeScript({ module: 'commonjs' }),
+    },
+
+    preprocessors: {
+      '**/*.js?(x)': file =>
+        require('@babel/core').transform(file.content, {
+          sourceMap: true,
+          compact: false,
+          filename: file.path,
+          presets: [require('babel-preset-jest')],
+        }),
+    },
+
+    debug: true,
+  };
+};
